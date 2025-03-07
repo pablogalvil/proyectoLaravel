ee9815145264dc87918c66ebec9cc44f00bae8d8
Pablo Galan <pablogalvil53@gmail.com>
2025-03-07 13:41:24 +0100

    nombres rutas

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,19 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/podcast', [PodcastController::class, 'index'])->name('podcast.index');
+    Route::get('/podcast', [PodcastController::class, 'indice'])->name('podcast.indice');
     //Ruta para el borrado del cliente actual
-    Route::delete('/podcast/{id}', [PodcastController::class, 'destroy'])->name('podcast.destroy');
+    Route::delete('/podcast/{id}', [PodcastController::class, 'eliminar'])->name('podcast.eliminar');
     //Ruta para cargar la vista de edicion del cliente seleccionado
-    Route::get('/podcast/{id}/edit', [PodcastController::class, 'edit'])->name('podcast.edit');
+    Route::get('/podcast/{id}/edit', [PodcastController::class, 'editar'])->name('podcast.editar');
     //Ruta para actualizar el cliente seleccionado
-    Route::put('/podcast/{id}', [PodcastController::class, 'update'])->name('podcast.update');
+    Route::put('/podcast/{id}', [PodcastController::class, 'actualizar'])->name('podcast.actualizar');
     //Ruta para cargar la vista de insercino
-    Route::get('/podcast/create', [PodcastController::class, 'create'])->name('podcast.create');
+    Route::get('/podcast/create', [PodcastController::class, 'crear'])->name('podcast.crear');
     //Ruta para insertar un nuevo cliente
-    Route::post('/podcast/store', [PodcastController::class, 'store'])->name('podcast.store');
+    Route::post('/podcast/store', [PodcastController::class, 'insertar'])->name('podcast.insertar');
     //Ruta para mostrar el cliente
-    Route::get('/podcast/show/{id}', [PodcastController::class, 'show'])->name('podcast.show');
+    Route::get('/podcast/show/{id}', [PodcastController::class, 'mostrar'])->name('podcast.mostrar');
 });
 
 
