d73648862811c5d2a9fe2ec770fdb644eee64a9c
pablogalvil <pablogalvil53@gmail.com>
2025-03-08 14:13:56 +0100

    cambios rutas 2

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ Route::middleware('auth')->group(function () {
     //Ruta para el borrado del cliente actual
     Route::delete('/podcast/{id}', [PodcastController::class, 'eliminar'])->name('podcast.eliminar');
     //Ruta para cargar la vista de edicion del cliente seleccionado
-    Route::get('/podcast/{id}/edit', [PodcastController::class, 'editar'])->name('podcast.editar');
+    Route::get('/podcast/{id}/editar', [PodcastController::class, 'editar'])->name('podcast.editar');
     //Ruta para actualizar el cliente seleccionado
     Route::put('/podcast/{id}', [PodcastController::class, 'actualizar'])->name('podcast.actualizar');
     //Ruta para cargar la vista de insercino
-    Route::get('/podcast/create', [PodcastController::class, 'crear'])->name('podcast.crear');
+    Route::get('/podcast/crear', [PodcastController::class, 'crear'])->name('podcast.crear');
     //Ruta para insertar un nuevo cliente
-    Route::post('/podcast/store', [PodcastController::class, 'insertar'])->name('podcast.insertar');
+    Route::post('/podcast/insertar', [PodcastController::class, 'insertar'])->name('podcast.insertar');
     //Ruta para mostrar el cliente
     Route::get('/podcast/mostrar/{id}', [PodcastController::class, 'mostrar'])->name('podcast.mostrar');
 });
