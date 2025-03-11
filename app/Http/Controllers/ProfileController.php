432208581a189ca180b5c541d136f8c5f373b5af
pablogalvil <pablogalvil53@gmail.com>
2025-03-11 20:14:32 +0100

    cambios imagen profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -30,14 +32,34 @@ class ProfileController extends Controller
     // Actualizar los datos del perfil
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        $user = User::findOrFail(Auth::user()->id);
+
         $request->user()->fill($request->validated());
-        
+
         // Verificar si el email ha cambiado
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-        
+
         $request->user()->save();
+
+        if ($request->hasFile('image')) {
+
+            //Eliminamos la imagen anterior
+            if ($user->image) {
+                Storage::disk('public')->delete($user->image);
+            }
+
+            //Definimos la ruta especifica para este Podcast
+            $carpetaUser = 'image/user' . $user->id;
+
+            //Guardamos la imagen en el hd y obtenemos la ruta completa
+            $imagePath = $request->file('image')->store($carpetaUser, 'public');
+
+            //Hay que añadir la ruta de la imagen a los datos que se van a 
+            //insertar en bd
+            $request->user()->update(['image' => $imagePath]);
+        }
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
